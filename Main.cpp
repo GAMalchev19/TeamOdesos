07cf133685bdd604b2ef391bbe9efb17ab9b4ae4
Teodor Osmanliev <TPOsmanliev19@codingburgas.bg>
2022-02-07 23:22:53 +0200

    Add mainMenu function

--- Main.cpp
+++ Main.cpp
@@ -14,7 +14,23 @@ void welcomeGreet()
 	cout << "###########################################################################################################" << endl;
 }
 
+
+void mainMenu()
+{
+	cout << "1. Play" << endl;
+	cout << "2. Settings" << endl;
+	cout << "3. Add Questions" << endl;
+	cout << "3. About Our Team" << endl;
+	cout << "4.	Quit" << endl;
+
+}
+
+
 int main()
 {
 	welcomeGreet();
+	
+	cout << endl;
+
+	mainMenu();
 }
