92e5bfcee645f2b1289bb2d05a9f63150cc6d3b0
Teodor Osmanliev <TPOsmanliev19@codingburgas.bg>
2022-02-08 19:58:53 +0200

    Change in function mainMenu menus and add test case for the choiceof menu

--- Main.cpp
+++ Main.cpp
@@ -18,10 +18,13 @@ void welcomeGreet()
 void mainMenu()
 {
 	cout << "1. Play" << endl;
-	cout << "2. Settings" << endl;
-	cout << "3. Add Questions" << endl;
+	cout << "2. How To Play" << endl;
 	cout << "3. About Our Team" << endl;
-	cout << "4.	Quit" << endl;
+	cout << "4. Quit" << endl;
+}
+
+void playMenu()
+{
 
 }
 
@@ -29,8 +32,41 @@ void mainMenu()
 int main()
 {
 	welcomeGreet();
-	
+
 	cout << endl;
 
 	mainMenu();
+
+	int choiceMenu;
+
+	cin >> choiceMenu;
+
+	if (choiceMenu == 1)
+	{
+
+	}
+	else if (choiceMenu == 2)
+	{
+
+	}
+	else if (choiceMenu == 3)
+	{
+
+	}
+	else if (choiceMenu == 4)
+	{
+
+	}
+	else
+	{
+		system("CLS");
+		cout << "Incorrect choice of menu! Please choose another." << endl;
+		system("pause");
+
+		system("CLS");
+
+		welcomeGreet();
+		mainMenu();
+		cin >> choiceMenu;
+	}
 }
