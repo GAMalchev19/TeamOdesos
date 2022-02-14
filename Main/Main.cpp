9e5897e9d3e9e13fa11720845c57a9c8f0663e33
Teodor Osmanliev <TPOsmanliev19@codingburgas.bg>
2022-02-14 23:53:56 +0200

    made function menuLink and remove unnecessary variables

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
-#include "questionHeader.h"
 using namespace std;
 
-// The name of the current player
-string namePlayer;
+// This is the player's choice of menu
+int choiceMenu;
 
 
-// This is the welcome greeting in the beggining of the program
+// This is the welcome greeting in the beginning of the program
 void welcomeGreet()
 {
-	cout << "###########################################################################################################" << endl;
-	cout << "   #                    #  ##########   #            ##########  #########  # #       # #  ##########  #" << endl;
-	cout << "    #                  #   #            #            #           #       #  #  #     #  #  #           #" << endl;
-	cout << "     #       ##       #    #            #            #           #       #  #   #   #   #  #           #" << endl;
-	cout << "      #     #  #     #     ##########   #            #           #       #  #    # #    #  ##########  #" << endl;
-	cout << "       #   #    #   #      #            #            #           #       #  #     #     #  #           #" << endl;
-	cout << "        # #      # #       #            #            #           #       #  #           #  #            " << endl;
-	cout << "         #        #        ##########   ###########  ##########  #########  #           #  ##########  #" << endl;
-	cout << "###########################################################################################################" << endl;
+    cout << "###########################################################################################################" << endl;
+    cout << "   #                    #  ##########   #            ##########  #########  # #       # #  ##########  #" << endl;
+    cout << "    #                  #   #            #            #           #       #  #  #     #  #  #           #" << endl;
+    cout << "     #       ##       #    #            #            #           #       #  #   #   #   #  #           #" << endl;
+    cout << "      #     #  #     #     ##########   #            #           #       #  #    # #    #  ##########  #" << endl;
+    cout << "       #   #    #   #      #            #            #           #       #  #     #     #  #           #" << endl;
+    cout << "        # #      # #       #            #            #           #       #  #           #  #            " << endl;
+    cout << "         #        #        ##########   ###########  ##########  #########  #           #  ##########  #" << endl;
+    cout << "###########################################################################################################" << endl;
 }
 
 // This is the main menu
 void mainMenu()
 {
-	cout << "                                             1. Play" << endl;
-	cout << "                                             2. How To Play" << endl;
-	cout << "                                             3. About Our Team" << endl;
-	cout << "                                             4. Quit" << endl;
+    cout << "                                             1. Play" << endl;
+    cout << "                                             2. How To Play" << endl;
+    cout << "                                             3. About Our Team" << endl;
+    cout << "                                             4. Quit" << endl;
 }
 
 // This is the function that will start the game
@@ -38,85 +37,75 @@ void playMenu()
 // This is the function after starting the game to input the name of the player
 void startGame()
 {
-	cout << "Welcome to the game! What is your first name?" << endl;
-
-	cin >> namePlayer;
-
 
 }
 
 // This the how to play menu that will inform the player to understand the game
 void howToPlayMenu()
 {
-	system("CLS");
-	cout << "Our game is like Who Wants to Be a Millionaire, but our questions are only historical. If you choose only the correct answers you will win our game. Let's try it!" << endl << endl;
-	system("pause");
-	system("CLS");
-	welcomeGreet();
-	mainMenu();
+    system("CLS");
+    cout << "Our game is like Who Wants to Be a Millionaire, but our questions are only historical. If you choose only the correct answers you will win our game. Let's try it!" << endl << endl;
+    system("pause");
+    system("CLS");
 }
 
 // This the function of the menu which will display information for our team
 void aboutUsMenu()
 {
-	system("CLS");
-	cout << "Scrum Trainer:         Frontend developer:         Backend developer:            Q&A:                       Helper" << endl;
-	cout << "Georgi Malchev          Teodor Osmanliev            Teodor Todorov        Kaloqn Pazlamachev            Andrey Andreev" << endl;
-	system("pause");
-	system("CLS");
-	welcomeGreet();
-	mainMenu();
-
+    system("CLS");
+    cout << "Scrum Trainer:         Frontend developer:         Backend developer:            Q&A:                       Helper" << endl;
+    cout << "Georgi Malchev          Teodor Osmanliev            Teodor Todorov        Kaloqn Pazlamachev            Andrey Andreev" << endl;
+    system("pause");
+    system("CLS");
 }
 
 // This is the function for ending the program
 void quitMenu()
 {
-	system("CLS");
+    system("CLS");
 }
+
+void menuLink(int choice)
+{
+    switch (choice)
+    {
+    case 1:
+    {
+        playMenu();
+        system("pause");
+        break;
+    }
+    case 2:
+    {
+        howToPlayMenu();
+        system("pause");
+        break;
+    }
+    case 3:
+    {
+        aboutUsMenu();
+        system("pause");
+        break;
+    }
+    }
+}
+
 
 int main()
 {
-	welcomeGreet();
+    while (choiceMenu != 4) {
+        welcomeGreet();
 
-	cout << endl;
+        cout << endl;
 
-	mainMenu();
+        mainMenu();
 
-	// This is the player's choice of menu
-	int choiceMenu;
+        cout << endl << "Choose a menu:";
+        cin >> choiceMenu;
 
-	cin >> choiceMenu;
-
-	if (choiceMenu == 1)
-	{
-
-	}
-	else if (choiceMenu == 2)
-	{
-		howToPlayMenu();
-	}
-	else if (choiceMenu == 3)
-	{
-		aboutUsMenu();
-	}
-	else if (choiceMenu == 4)
-	{
-		quitMenu();
-	}
-	// This else is for incorrect choice of menu
-	else
-	{
-		system("CLS");
-		cout << "Incorrect choice of menu! Please choose another." << endl;
-		system("pause");
-
-		system("CLS");
-
-		welcomeGreet();
-		mainMenu();
-		cin >> choiceMenu;
-	}
+        menuLink(choiceMenu);
+    }
+    quitMenu();
 }
 
 
